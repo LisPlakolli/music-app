d8cc00eb2017c0057c0065751bdf22b39febd74d
LisPlakolli <plakollilis@gmail.com>
2026-03-16 12:47:26 -0400

    redoing file structure

--- backend/src/main/java/musicapp/MusicApp.java
+++ backend/src/main/java/musicapp/MusicApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class musicApp {
+public class MusicApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(musicApp.class, args);
+		SpringApplication.run(MusicApp.class, args);
 	}
 
 }
